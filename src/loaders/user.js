4ccc9bf0ca60f99bd9edc1e212379c04a66c884b
andyliwr <andyliwr@outlook.com>
2019-01-15 21:27:07 +0800

    finished graphql learning

---
+++ src/loaders/user.js
@@ -0,0 +1,11 @@
+const batchUsers = async (keys, models) => {
+  const users = await models.User.findAll({
+    where: {
+      id: {
+        $in: keys
+      }
+    }
+  })
+
+  return keys.map(key => users.find(user => user.id === key))
+}
